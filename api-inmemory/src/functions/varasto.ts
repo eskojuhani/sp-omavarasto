f92ae8b523adef3317dadf9f0a63bbc4d7bf5a07
eskojuhani <eskojuhani@gmail.com>
2024-12-04 12:46:28 +0200

    dev

--- api-inmemory/src/functions/varasto.ts
+++ api-inmemory/src/functions/varasto.ts
@@ -1,16 +1,18 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext, output } from '@azure/functions';
+import { app, HttpRequest, HttpResponseInit, input, InvocationContext, output } from '@azure/functions';
 
-const sqlVarastoOutput = output.sql({
+/* Varasto - Create */
+
+const sqlVarastoCreate = output.sql({
   commandText: 'dbo.Varasto',
   connectionStringSetting: 'SqlConnectionString',
 });
 
-export async function varastoTrigger(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+export async function varastoInsert(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
   context.log('HTTP + SQL for Varasto.');
 
   const body = await request.text();
   console.log("Varasto:", JSON.stringify(body));
-  context.extraOutputs.set(sqlVarastoOutput, body);
+  context.extraOutputs.set(sqlVarastoCreate, body);
 
   return { status: 201 };
 }
@@ -18,6 +20,56 @@ export async function varastoTrigger(request: HttpRequest, context: InvocationCo
 app.http('varasto', {
   methods: ['POST'],
   authLevel: 'anonymous',
-  extraOutputs: [sqlVarastoOutput],
-  handler: varastoTrigger,
+  extraOutputs: [sqlVarastoCreate],
+  handler: varastoInsert,
 });
+
+/* Varasto - Delete */
+
+const sqlDeleteOne = input.generic({
+  type: 'sql',
+  commandText: 'delete from dbo.Varasto where Id = @Id',
+  commandType: 'Text',
+  parameters: '@Id={id}',
+  connectionStringSetting: 'SqlConnectionString'
+})
+
+app.http('varasto', {
+  methods: ['DELETE'],
+  authLevel: 'anonymous',
+  route: 'varasto/{id}',
+  extraInputs: [sqlDeleteOne],
+  handler: async (request, context) => {
+      const products = JSON.stringify(context.extraInputs.get(sqlDeleteOne));
+      console.log("products", products);
+      return {
+          status: 201
+      };
+  }
+});
+
+/* Varastot - GetAll */
+const sqlGetAll = input.generic({
+    type: 'sql',
+    commandText: 'select * from dbo.Varasto where AsiakasId = @Id',
+    commandType: 'Text',
+    parameters: '@Id={id}',
+    connectionStringSetting: 'SqlConnectionString'
+})
+
+app.http('varastot', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    route: 'varastot/{id}',
+    extraInputs: [sqlGetAll],
+    handler: async (request, context) => {
+        const products = JSON.stringify(context.extraInputs.get(sqlGetAll));
+
+        return {
+            status: 200,
+            body: products
+        };
+    }
+});
+
+/*  */
